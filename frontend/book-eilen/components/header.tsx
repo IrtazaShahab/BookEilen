c8c8f3dc33fa9693ee365e0179741426c6e9c87a
Muaz Rehan <muazr.dev@gmail.com>
2025-06-27 00:47:41 +0500

    Font family set to motserrat, colors settings

--- frontend/book-eilen/components/header.tsx
+++ frontend/book-eilen/components/header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 "
                     >
                         <span className="sr-only">Open main menu</span>
                         <Bars3Icon aria-hidden="true" className="size-6" />
@@ -63,9 +63,9 @@ export default function Header() {
                 </div>
                 <PopoverGroup className="hidden lg:flex lg:gap-x-12">
                     <Popover className="relative">
-                        <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+                        <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold ">
                             Product
-                            <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
+                            <ChevronDownIcon aria-hidden="true" className="size-5 flex-none " />
                         </PopoverButton>
 
                         <PopoverPanel
@@ -79,14 +79,14 @@ export default function Header() {
                                         className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
                                     >
                                         <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                            <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
+                                            <item.icon aria-hidden="true" className="size-6  group-hover:text-indigo-600" />
                                         </div>
                                         <div className="flex-auto">
-                                            <a href={item.href} className="block font-semibold text-gray-900">
+                                            <a href={item.href} className="block font-semibold ">
                                                 {item.name}
                                                 <span className="absolute inset-0" />
                                             </a>
-                                            <p className="mt-1 text-gray-600">{item.description}</p>
+                                            <p className="mt-1 ">{item.description}</p>
                                         </div>
                                     </div>
                                 ))}
@@ -96,9 +96,9 @@ export default function Header() {
                                     <a
                                         key={item.name}
                                         href={item.href}
-                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
+                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold  hover:bg-gray-100"
                                     >
-                                        <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
+                                        <item.icon aria-hidden="true" className="size-5 flex-none " />
                                         {item.name}
                                     </a>
                                 ))}
@@ -106,18 +106,18 @@ export default function Header() {
                         </PopoverPanel>
                     </Popover>
 
-                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                    <a href="#" className="text-sm/6 font-semibold ">
                         Features
                     </a>
-                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                    <a href="#" className="text-sm/6 font-semibold ">
                         Marketplace
                     </a>
-                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                    <a href="#" className="text-sm/6 font-semibold ">
                         Company
                     </a>
                 </PopoverGroup>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                    <a href="#" className="text-sm/6 font-semibold ">
                         Log in <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
@@ -134,7 +134,7 @@ export default function Header() {
                                 className="h-8 w-auto"
                             />
                         </a>
-                        <button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 text-gray-700">
+                        <button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 ">
                             <span className="sr-only">Close menu</span>
                             <XMarkIcon aria-hidden="true" className="size-6" />
                         </button>
@@ -143,7 +143,7 @@ export default function Header() {
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 <Disclosure as="div" className="-mx-3">
-                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold  hover:bg-gray-50">
                                         Product
                                         <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
                                     </DisclosureButton>
@@ -153,37 +153,25 @@ export default function Header() {
                                                 key={item.name}
                                                 as="a"
                                                 href={item.href}
-                                                className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                                className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold  hover:bg-gray-50"
                                             >
                                                 {item.name}
                                             </DisclosureButton>
                                         ))}
                                     </DisclosurePanel>
                                 </Disclosure>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                                >
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50">
                                     Features
                                 </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                                >
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50">
                                     Marketplace
                                 </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                                >
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50">
                                     Company
                                 </a>
                             </div>
                             <div className="py-6">
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                                >
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold  hover:bg-gray-50">
                                     Log in
                                 </a>
                             </div>
