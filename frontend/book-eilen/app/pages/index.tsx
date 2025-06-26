41639bf23a9265f1cedf2f479efd8167e30d185f
Muaz Rehan <muazr.dev@gmail.com>
2025-06-26 23:47:00 +0500

    Scss configured

---
+++ frontend/book-eilen/app/pages/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Index() {
+    // const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    return <div className="bg-dark-900 text-white"></div>;
+}
