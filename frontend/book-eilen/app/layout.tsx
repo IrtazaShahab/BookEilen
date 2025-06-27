cff9c7adb4def4e73a2c2b5231561aa03b9d3b7f
Muaz Rehan <muazr.dev@gmail.com>
2025-06-28 00:31:34 +0500

    carousel added

--- frontend/book-eilen/app/layout.tsx
+++ frontend/book-eilen/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import '../src/styles/styles.scss';
-import Header from '@/components/header';
+import Header from '../global-components/header';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
--- frontend/book-eilen/app/pages/index/components/home-carousel.tsx
+++ frontend/book-eilen/app/pages/index/components/home-carousel.tsx
@@ -2,10 +2,33 @@
 
 import React from 'react';
 import { Carousel } from 'antd';
-import { useState } from 'react';
+
+const contentStyle: React.CSSProperties = {
+    margin: 0,
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
 export default function BeHomeCarousel() {
-    // const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-    return <div className="bg-dark-900"></div>;
+    return (
+        <div className="">
+            <Carousel arrows infinite={false}>
+                <div>
+                    <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>4</h3>
+                </div>
+            </Carousel>
+        </div>
+    );
 }
