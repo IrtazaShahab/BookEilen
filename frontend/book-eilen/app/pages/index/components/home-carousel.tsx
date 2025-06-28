6701ba57b893c2c9c1eaa8623dc03626ca9be9f7
Muaz Rehan <muazr.dev@gmail.com>
2025-06-28 18:39:03 +0500

    Image paths setup

--- frontend/book-eilen/app/pages/index/components/home-carousel.tsx
+++ frontend/book-eilen/app/pages/index/components/home-carousel.tsx
@@ -2,31 +2,24 @@
 
 import React from 'react';
 import { Carousel } from 'antd';
-
-const contentStyle: React.CSSProperties = {
-    margin: 0,
-    // height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-};
+import Image from 'next/image';
+import sampleCarouselImage from 'images/sample-carousel.jpg';
 
 export default function BeHomeCarousel() {
     return (
         <div className="be-home-carousel">
             <Carousel arrows infinite={false}>
                 <div>
-                    <img src="/frontend/book-eilen/assets/images/sample-carousel.jpg" alt="1" />
+                    <Image src={sampleCarouselImage} width="1000" height="100" alt="1" />
                 </div>
                 <div>
-                    <img src="/frontend/book-eilen/assets/images/sample-carousel.jpg" alt="2" />
+                    <Image src={sampleCarouselImage} width="1000" height="100" alt="1" />
                 </div>
                 <div>
-                    <img src="/frontend/book-eilen/assets/images/sample-carousel.jpg" alt="3" />
+                    <Image src={sampleCarouselImage} width="1000" height="100" alt="1" />
                 </div>
                 <div>
-                    <img src="/frontend/book-eilen/assets/images/sample-carousel.jpg" alt="4" />
+                    <Image src={sampleCarouselImage} width="1000" height="100" alt="1" />
                 </div>
             </Carousel>
         </div>
