a5b53495cf9a031ed87161fad63e326e0c4b22e7
Irtaza <163323264+IrtazaShahab@users.noreply.github.com>
2025-06-30 17:04:06 +0500

    header styling in-progress

--- frontend/book-eilen/global-components/header.tsx
+++ frontend/book-eilen/global-components/header.tsx
@@ -39,14 +39,14 @@ export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
-        <header className="bg-dark-900 be-header">
+        <header className="bg-dark-900 be-header max-w-7xl mt-16 fixed left-78 right-0 top-0 rounded-header">
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
                         <img
                             alt=""
-                            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                            src="/frontend/book-eilen/logo.svg"
                             className="h-8 w-auto"
                         />
                     </a>
@@ -109,12 +109,7 @@ export default function Header() {
                     <a href="#" className="text-sm/6 font-semibold ">
                         Features
                     </a>
-                    <a href="#" className="text-sm/6 font-semibold ">
-                        Marketplace
-                    </a>
-                    <a href="#" className="text-sm/6 font-semibold ">
-                        Company
-                    </a>
+
                 </PopoverGroup>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <a href="#" className="text-sm/6 font-semibold ">
